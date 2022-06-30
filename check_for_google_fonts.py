89965cffc3c063e16210024cba74a7bb8f923028
Sebastian Weiß <sw@3ele.de>
2022-06-30 14:09:15 +0200

    clean code

--- check_for_google_fonts.py
+++ check_for_google_fonts.py
@@ -1,47 +1,52 @@
+#!/usr/bin/env python 
 import requests
 from bs4 import BeautifulSoup
 import csv
 import requests
 import sys
 import re
-csv_file = sys.argv[1]
-print(csv_file)
-results = []
-with open(csv_file, 'r') as csvFile:
-    reader = csv.reader(csvFile, delimiter=',', quotechar='|')
-    for row in reader:
-        #https://lightweb-media.de
-        try: 
-            r = requests.get('https://' + row[0] )
-            if (r.status_code == 200):
-                
-                try:
-                    soup = BeautifulSoup(r.content, "html.parser")
-                    links = soup.findAll('link',{'href': re.compile(r'fonts.google')})
-                    findings =[]
-                    for x in links:
-                        findings.append(str(x))
-          
-                    
-                    if (len(links) > 0):
-                        result = {
-                            'url' : row[0],
-                            'links' :  "|".join(findings)
-                        }
-                        results.append(result)
-                      
 
-                except Exception as e:
-                    print (e)
-        except Exception as e:
-            print (e)
+def scan_website(domain):
     
+    try: 
+                r = requests.get('https://' + domain )
+                if (r.status_code == 200):
+                    
+                    try:
+                        soup = BeautifulSoup(r.content, "html.parser")
+                        links = soup.findAll('link',{'href': re.compile(r'fonts.google')})
+                        findings =[]
+                        for x in links:
+                            findings.append(str(x))
+                        if (len(links) > 0):
+                            result = {
+                                'url' : domain,
+                                'links' :  "|".join(findings)
+                            }
+                            return result
+                    except Exception as e:
+                        print (e)
+    except Exception as e:
+                print (e)
 
-with open('result.csv', 'w') as csvfile:
-        fieldnames = ['url', 'links']
-        writer = csv.DictWriter(csvfile, fieldnames)
-        for data in results:
-            writer.writerow(data)
 
 
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        csv_file = sys.argv[1]
+        with open(csv_file, 'r') as csvFile:
+            reader = csv.reader(csvFile, delimiter=',', quotechar='|')
+            for row in reader:
+                results = []
+                result = scan_website(row[0])
+                if result:
+                    results.append()
+
+        csv_output_file = sys.argv[2]   
+        with open(csv_output_file, 'w') as csvfile:
+                fieldnames = ['url', 'links']
+                writer = csv.DictWriter(csvfile, fieldnames)
+                for data in results:
+                    writer.writerow(data)
 
